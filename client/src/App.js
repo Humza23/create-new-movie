952998ec61108a2211ace59b899adb8c4fc9d583
Humza23 <79542316+Humza23@users.noreply.github.com>
2021-05-20 21:49:54 -0400

    try10

---
+++ client/src/App.js
@@ -0,0 +1,84 @@
+import React, { useEffect, useState } from "react";
+
+import { Route, Switch, Redirect } from "react-router-dom";
+import MovieList from './components/MovieList';
+import Movie from './components/Movie';
+
+import MovieHeader from './components/MovieHeader';
+
+import EditMovieForm from './components/EditMovieForm';
+import FavoriteMovieList from './components/FavoriteMovieList';
+
+import AddMovieForm from './components/AddMovieForm'
+
+import axios from 'axios';
+
+const App = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/api/movies')
+      .then(res => {
+        setMovies(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
+  const deleteMovie = (id)=> {
+    axios.delete(`http://localhost:5000/api/movies/${id}`)
+    .then(res=>{
+      console.log(res.data);
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+  }
+
+  const addToFavorites = (movie) => {
+    
+  }
+
+  return (
+    <div>
+      <nav className="navbar navbar-dark bg-dark">
+        <span className="navbar-brand" ><img width="40px" alt="" src="./Lambda-Logo-Red.png"/> HTTP / CRUD Module Project</span>
+      </nav>
+
+      <div className="container">
+        <MovieHeader/>
+        <div className="row ">
+          <FavoriteMovieList favoriteMovies={favoriteMovies}/>
+        
+          <Switch>
+            <Route path="/movies/edit/:id" render={(props)=> {
+              return(<EditMovieForm {...props} setMovies={setMovies}/>)
+            }}/>
+           
+            <Route path="/movies/:id" render={(props)=> {
+              return (<Movie {...props} setMovies={setMovies} deleteMovie={deleteMovie} />)
+            }}/>
+
+            <Route path="/movies">
+              <MovieList movies={movies}/>
+            </Route>
+
+            <Route path="/addmovie" render={(props) => {
+              return (<AddMovieForm {...props} setMovies={setMovies}/>)
+            }}/>
+
+            <Route path="/">
+              <Redirect to="/movies"/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default App;
+
